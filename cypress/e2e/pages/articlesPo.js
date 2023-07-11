2f735a8cf3b02e56a5f6ae9add68304d7dfe045f
redabenabdallah <reda.benabdallah@yahoo.fr>
2023-07-11 15:13:54 +0200

    Amelioration du test sur le choix max et min des articles

--- cypress/e2e/pages/articlesPo.js
+++ cypress/e2e/pages/articlesPo.js
@@ -1,11 +1,19 @@
 export class ArticlesPo {
 
     chooseArticleMaxMin() {
-        cy.get('body').find('[class="inventory_item_price"]').contains('7.99').eq(0).next('button').click()
-        cy.get('body').find('[class="inventory_item_price"]').contains('49.99').eq(0).next('button').click()
+        const listeItemPrix = new Array()
+        cy.get('body').find('[class="inventory_item_price"]').each((item) => {
+            listeItemPrix.push(parseFloat(item.text().replace("$", "").trimEnd().trimStart()))
+        }).wrap(listeItemPrix).then((listeItemPrix) => {
+            console.log('listeItemPrix ' + listeItemPrix)
+            cy.get('body').find('[class="inventory_item_price"]').contains(listeItemPrix.sort(function(a,b){return a -b}).at(0)).eq(0).next('button').click()
+            cy.get('body').find('[class="inventory_item_price"]').contains(listeItemPrix.sort(function(a,b){return a -b}).at(listeItemPrix.length - 1)).eq(0).next('button').click()
+        })
+        //cy.get('body').find('[class="inventory_item_price"]').contains('7.99').eq(0).next('button').click()
+        //cy.get('body').find('[class="inventory_item_price"]').contains('49.99').eq(0).next('button').click()
     }
 
-    verifPanier(nbItem){
+    verifPanier(nbItem) {
         cy.get('[class="shopping_cart_badge"]').should('have.text', nbItem)
     }
 }
